ed3ccd27c856479f0ff1f5fa98ea5f781feed226
Supratik Das <supratik@Supratiks-MacBook-Air.local>
2025-09-12 02:30:42 +0530

    vite config  updated

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/bolt-ai-my-port-folio",
+  base: "/bolt-ai-my-port-folio/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
